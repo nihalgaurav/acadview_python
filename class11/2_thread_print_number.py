e69916574ee53ee5af2b267b9717a3d9ff3e994e
nihalgaurav <nihalgaurav85@gmail.com>
2018-06-22 12:16:36 +0530

    assignment 2 class11 added

---
+++ class11/2_thread_print_number.py
@@ -0,0 +1,12 @@
+#Make a thread that prints numbers from 1-10, waits for 1 sec between
+
+import threading
+from threading import Thread
+import time
+
+def display():
+    for x in range(1,11):
+        print(x)
+        time.sleep(1)
+t=Thread(target=display)
+t.start()
