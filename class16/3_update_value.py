7cd9f220ef0094f0624e592200ecf3adc707aa32
nihalgaurav <nihalgaurav85@gmail.com>
2018-07-02 16:19:31 +0530

    all assignment class 16 committed

---
+++ class16/3_update_value.py
@@ -0,0 +1,15 @@
+# Update any values in any of the tables. Print the original and updated values.
+
+import pymysql as py
+
+db=py.connect("localhost","root","nihal@85210","library")
+cursor=db.cursor()
+cursor.execute("select * from book")
+result=cursor.fetchall()
+print(result)
+cursor.execute("update book set genre='romance' where book_id=001")
+cursor.execute("select * from book")
+result=cursor.fetchall()
+print(result)
+db.commit()
+db.close()
